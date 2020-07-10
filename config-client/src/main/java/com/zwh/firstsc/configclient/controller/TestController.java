24dbfdbcc31dd46aa2d8ef33f8743f8d8c0fd0e3
FuckMeee <471567032@qq.com>
2020-07-10 15:28:01 +0800

    1.0

---
+++ config-client/src/main/java/com/zwh/firstsc/configclient/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.zwh.firstsc.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Value("${firstsc.name}")
+//    @Value("${server.port}")
+    private String str;
+
+    @GetMapping("/test01")
+    public String test01() {
+        return str;
+    }
+}
